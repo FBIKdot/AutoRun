ea3985b5919996eb32b5ed802fa18293bd101abd
FBIK <bovinefbik@foxmail.com>
2022-12-09 22:54:41 +0800

    优化

--- autorun.js
+++ autorun.js
@@ -3,13 +3,12 @@ const process = require("process");
 const fs = require("fs");
 
 
-var configFile = fs.readFileSync("./config.json", "utf8");
-var config = JSON.parse(configFile);
+var config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
 
 const DEBUG = config.enabledebug;
 function debugConsoleLog(text, text2) {
     if (DEBUG === true) {
-        console.log("\033[32m[DEBUG]\033[0m", text, (text2 == undefined) ? "" : text2);
+        console.log("\033[32m[DEBUG]\033[0m", text, text2 == undefined ? "" : text2);
     };
 };
 debugConsoleLog("ENABLED");
@@ -19,7 +18,7 @@ var timeList = new Map(config.timeList);
 var playCmd = config.playCmd;
 var processKillCmd = config.processKillCmd;
 var timeOut = config.timeOut;
-timeOut = (Number(timeOut) < 1000 || Object.is(Number(timeOut),NaN)) ? 1000 : Number(timeOut);
+timeOut = Number(timeOut) < 1000 || Object.is(Number(timeOut), NaN) ? 1000 : Number(timeOut);
 debugConsoleLog("timeList:", timeList);
 debugConsoleLog("timeOut:", timeOut);
 
@@ -40,8 +39,8 @@ function again() {
     if (processKillCmd === false) {
         run();
     } else {
-        childProcess.exec(processKillCmd, (err) => {
-            console.log((err) ? "\033[31m[WARN]\033[0m " + "子进程关闭失败" : "子进程关闭成功");
+        childProcess.exec(processKillCmd, err => {
+            console.log(err ? "\033[31m[WARN]\033[0m " + "子进程关闭失败" : "子进程关闭成功");
             run();
         });
     };
@@ -71,18 +70,18 @@ function getTargetTime() {
         console.log("\033[31m[WARN]\033[0m " + "目标时间均已过,脚本停止");
         process.exit();
     };
-    debugConsoleLog("target: " + ((sh > 9) ? sh : "0" + sh) + ":" + ((sm > 9) ? sm : "0" + sm));
+    debugConsoleLog("target: " + sh > 9 ? sh : "0" + sh + ":" + sm > 9 ? sm : "0" + sm);
     return sh + ":" + sm;
 };
 
 function getleftTime(setTime) {
     let now = new Date(), h = now.getHours(), m = now.getMinutes();
-    let x = setTime.split(":")[0], y = setTime.split(":")[1];
+    let [x, y] = setTime.split(":");
     return 60 * (Number(x) - h) + (Number(y) - m);
 }
 
 function main() {
-    let sh = targetTime.split(":")[0], sm = targetTime.split(":")[1];
+    let [sh, sm] = targetTime.split(":");
     let leftTime = getleftTime(targetTime);
     let now = new Date(),
         h = now.getHours(),
@@ -95,13 +94,13 @@ function main() {
         setTimeout(again, timeOut);
     } else {
         let output = "现在是";
-        output += (h < 10) ? "0" + h + ":" : h + ":";
-        output += (m < 10) ? "0" + m + ":" : m + ":";
-        output += (s < 10) ? "0" + s : s;
+        output += h < 10 ? "0" + h + ":" : h + ":";
+        output += m < 10 ? "0" + m + ":" : m + ":";
+        output += s < 10 ? "0" + s : s;
         output += ",目标";
-        output += ((sh > 9) ? sh : "0" + sh) + ":" + ((sm > 9) ? sm : "0" + sm) + ",剩余" + leftTime + "min";
+        output += sh > 9 ? sh : "0" + sh + ":" + sm > 9 ? sm : "0" + sm + ",剩余" + leftTime + "min";
         console.log(output);
-        setTimeout(main, (leftTime > 1) ? 60000 : 1000);
+        setTimeout(main, leftTime > 1 ? 60000 : 1000);
     };
 };
 
